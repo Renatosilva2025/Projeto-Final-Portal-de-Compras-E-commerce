2d523c1d9e98db0f76a4ba0f582de266acf421e9
Renato De Almeida Silva <193662907+Renatosilva2025@users.noreply.github.com>
2026-08-09 01:06:00 +0000

    Before: consegui pelo caminho 2

--- isolate/assets/skeleton-CDNZcgIA.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./framer-motion-D5IBGcks.js";import{c as s}from"./button-BscBFIPT.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
